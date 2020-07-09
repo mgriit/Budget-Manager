6d465a2780fa4e363ac5db90332e4e9afbdfef5c
M G Rabbany <M G Rabbany@DESKTOP-8SPU0OU>
2020-07-09 10:11:23 +0600

    added entity

---
+++ Budget Manager/Entities/Code.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Budget_Manager.Entities
+{
+    public class Code
+    {
+        public Int64 CodeId { get; set; }
+        public String CodeName { get; set; }
+        public String CodeNumber { get; set; }
+        public int? SerialNo { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+        public Int64 Creator { get; set; }
+        public Int64? Modifier { get; set; }
+
+    }
+}
